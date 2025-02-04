456b4a8c9f497a3422d6bad08af2ce0b48624873
Dohoon <peterdhp0205@gmail.com>
2025-02-04 18:57:59 +0900

    actual loss contract update

--- LLMengine_split_0110.py
+++ LLMengine_split_0110.py
@@ -142,12 +142,12 @@ This is useful for finding context or specific information related to insurance 
             insurance_start_date = contract["resCoverageLists"][0]["commStartDate"]
             matching_insurance_text = render_policy_as_table_actual(matching_contract)
             break
-    #print(insurance_company)
+    print(insurance_company)
     insurance_company_code_dict = {"메리츠화재보험" : "0101" , "한화손해보험" : "0102", "삼성화재해상보험" : "0108", "DB손해보험" : "0111", "NH농협손해보험" : "0171", "삼성생명보험" : "0203","현대해상화재보험" : "0109"}
     insurance_company_code = insurance_company_code_dict.get(insurance_company, "Unknown")
     
     
-    #print("insurance_company_code is ", insurance_company_code)
+    print("insurance_company_code is ", insurance_company_code)
     
     
     # insurance_name을 이용해서 insurance_enrollment_info에서 해당 보험에 대한 정보를 가져올 옴
@@ -167,10 +167,15 @@ This is useful for finding context or specific information related to insurance 
             insurance_name_normalized = unicodedata.normalize('NFC', insurance_name)
             names_normalized = [unicodedata.normalize('NFC', c) for c in names]
             closest_match = get_close_matches(insurance_name_normalized, names_normalized, n=3)
+            print(insurance_name_normalized)
+            print(names_normalized)
+            print(closest_match)
 
             if closest_match:
+                
                 matching_items = [item for item in loaded_company_toc if unicodedata.normalize('NFC', item['name']) == closest_match[0]]
 
+        
         if matching_items == []:  # If no valid items found, return None
             return "해당 약관에 대한 정보가 조회 불가능합니다. 약관 정보가 없는 선에서 최대한 답변을 주고 서비스 업데이트를 기다려달라는 안내해줘"
         
