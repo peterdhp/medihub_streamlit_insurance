8daec1c46822faa6a7e16bb2660e64e525eec3c3
Dohoon <peterdhp0205@gmail.com>
2025-02-24 15:14:47 +0900

    update

--- pages/w_insurance.py
+++ pages/w_insurance.py
@@ -19,13 +19,23 @@ if "birth" not in st.session_state:
 if "messages_w" not in st.session_state:
     st.session_state["messages_w"] = [{"type": "ai", "content": "보험과 관련해서 어떤게 궁금하신가요?"}]
     #st.session_state.thread_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+if 'log_str' not in st.session_state:
+    st.session_state['log_str'] = 'ai: 보험과 관련해서 어떤게 궁금하신가요?'
 
 
 def reset():
     st.session_state["messages_w"] = [{"type": "ai", "content": "보험과 관련해서 어떤게 궁금하신가요?"}]
 
+    
+
 with st.sidebar :
     st.button("대화 내역 초기화 하기", on_click=reset)
+    st.download_button(
+                label="대화내역 저장하기",
+                data=st.session_state.log_str,
+                file_name=datetime.datetime.now().strftime("%Y%m%d%H%M%S")+'_보장곰 내역.txt',
+                mime='text/plain'
+            )
 
 @st.cache_resource
 def init_connection():
@@ -66,6 +76,7 @@ if prompt := st.chat_input():
 
     st.session_state.messages_w.append({"type": "human", "content": prompt})
     st.chat_message("human").write(prompt)
+    st.session_state.log_str += 'human: ' + prompt
     with collect_runs() as cb:
         with st.spinner('보장곰이 가입하신 보험들을 살펴보고 있습니다.'):
             response = insurance_engine.invoke({"user_input": prompt, "insurance_enrollment_info" : insurance_enrollment_info, "chat_history": st.session_state.messages_w},config={"recursion_limit": 15})
@@ -74,23 +85,32 @@ if prompt := st.chat_input():
     if response['non_related'] == 'F' :
         st.session_state.messages_w.append({"type": "ai", "content": "저는 건강보험 관련 질문에 대해서만 답변할 수 있어요."})
         st.chat_message("ai").write("저는 건강보험 관련 질문에 대해서만 답변할 수 있어요.")
+        st.session_state.log_str += 'ai: ' + "저는 건강보험 관련 질문에 대해서만 답변할 수 있어요." + '\n'
     else :
         msg = response["response"]
         details = response.get("report")
         st.session_state.messages_w.append({"type": "ai", "content": msg})
         st.chat_message("ai").write(msg)
+        st.session_state.log_str += 'ai: ' + msg + '\n'
+        
         
         if details :
             with st.expander("See explanation"):
                 details_str = "\n\n".join(f"{key}: {value}" for key, value in details.items())
                 st.write(details_str)
+                st.session_state.log_str += '\n\nreport:\n' + st.session_state.details_str + '\n'
+                
    
     
     if 'end_of_session' in response :    
         if response['end_of_session'] != 'continue' :
             st.session_state.messages_w.append({"type": "ai", "content": "궁금한 점이 잘 해소되었나요? \n 더 질문을 하셔도 좋고 상담 요약을 전달드릴 수 있어요.\n [end_of_session token] : " + response['end_of_session']})
             st.chat_message("ai").write("궁금한 점이 잘 해소되었나요? \n 더 질문을 하셔도 좋고 상담 요약을 전달드릴 수 있어요.\n [end_of_session token] : " + response['end_of_session'])
-            
+            st.session_state.log_str += 'ai: 궁금한 점이 잘 해소되었나요? 더 질문을 하셔도 좋고 상담 요약을 전달드릴 수 있어요.' + '\n'
+
+    
+if st.session_state.details_str != '' :
+    st.session_state.log_str += + '\n\n' + st.session_state.details_str 
             
 if st.session_state.get("run_id"):
     run_id = st.session_state.run_id
